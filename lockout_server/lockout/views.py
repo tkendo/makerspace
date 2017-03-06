b0f771cfc49ba100069f7ac3b532b668af6a0144
tkendo <thomaskendo@gmail.com>
2017-03-06 04:07:17 +0000

    Checking in some initial database setup and testcode

--- lockout_server/lockout/views.py
+++ lockout_server/lockout/views.py
@@ -16,7 +16,6 @@ from lockout.functions import *
 
 import sys
 
-i = 0
 
 #@app.route('/')
 #def index():
@@ -25,17 +24,42 @@ i = 0
 
 @app.route('/unlock', methods=['POST'])
 def send_unlock():
-    global i
-#    print "%d" % i
     if request.method == 'POST':
         sys.stdout.write("RX Request: ip=" + request.remote_addr)
         sys.stdout.write(" data=" + request.get_data(as_text=True) + "\n")
-        print request.form['arg']
-        i = not i 
-    
-        if i == 1:
-            return "status\n1"
-        return "beepflash\n1" 
-    else:
-        print "not post"
+        sys.stdout.write("arg=" + request.form['arg'] + "\n")
+
+        status = get_node_status(request.remote_addr)
+
+        reply = "status\n0"
+        
+        if status == None:
+            print "Machine not found!" # TODO handle error
+        else:
+            reply = "status\n" + str(status[0])
+
+        sys.stdout.write("reply=" + reply + "\n")
+        return reply
+
+#        return "beepflash\n1" 
+
+@app.route('/setlock', methods=['POST'])
+def set_lock():
+    if request.method == 'POST':
+        sys.stdout.write("SetLock Request. ip=" + request.remote_addr)
+        sys.stdout.write(" node=" + request.form['node'])
+        sys.stdout.write(" lock=" + request.form['lock'] + "\n")
+
+        node = int(request.form['node'])
+        setval = int(request.form['lock'])
+
+        if setval == 1 or setval == 0:
+            retval = set_node_status(node, setval)
+            if retval != 1:
+                print "Database error"
+        else:
+            retval = 0
+            print "Invalid lock value specified"
+
+        return str(retval) 
 
